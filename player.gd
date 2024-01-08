1a306d8c44698b6784c556b587dfd56c735b9d5d
YuunsGit <yunus.emre.kepenek@outlook.com>
2024-01-09 00:35:15 +0300

    Add multiplayer

--- player.gd
+++ player.gd
@@ -3,11 +3,19 @@ extends CharacterBody2D
 @export var SPEED = 300.0
 @onready var animation_player = $AnimationPlayer
 
+func _enter_tree():
+	set_multiplayer_authority(name.to_int())
+
 func _process(delta):
-	#global_rotation = global_position.direction_to(get_global_mouse_position()).angle() + PI/2.0
-	look_at(get_global_mouse_position())
+	if is_multiplayer_authority():
+		look_at(get_global_mouse_position())
+		if Input.is_action_pressed("primary"):
+			animation_player.play("sword_slash", -1, 2.25)
+		if Input.is_action_pressed("secondary"):
+			animation_player.play("shield_raise")
 
 func _physics_process(delta):
-	var move_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = move_dir * SPEED
-	move_and_slide()
+	if is_multiplayer_authority():
+		var move_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+		velocity = move_dir * SPEED
+		move_and_slide()
