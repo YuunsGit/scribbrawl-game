b5600e58645f59b49758a7e81c3dcfe9af8a8bd8
YuunsGit <yunus.emre.kepenek@outlook.com>
2024-01-08 22:29:17 +0300

    Build test map

--- player.gd
+++ player.gd
@@ -4,11 +4,10 @@ extends CharacterBody2D
 @onready var animation_player = $AnimationPlayer
 
 func _process(delta):
-	global_rotation = global_position.direction_to(get_global_mouse_position()).angle() + PI/2.0
+	#global_rotation = global_position.direction_to(get_global_mouse_position()).angle() + PI/2.0
+	look_at(get_global_mouse_position())
 
 func _physics_process(delta):
 	var move_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = move_dir * SPEED
-	if velocity.normalized().length() > 0:
-		animation_player.play("walk")
 	move_and_slide()
